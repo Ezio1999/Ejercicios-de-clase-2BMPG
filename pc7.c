488b5f1c07dfcf67cad43972f86ab70587b8d493
Castillo <=joseignacionavacastillo@gmail.com>
2024-04-29 19:26:32 -0600

    Se agrego un hola mundo al pc7

--- pc7.c
+++ pc7.c
@@ -1,30 +1,35 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int x,b,B,h;
-    float z; 
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x, b, B, h;
+    float z;
     printf("CALCULO DE AREAS\n");
     printf("____________________\n");
     printf("1.Calcular el area de un triangulo.\n");
     printf("2.Calcular el area de un trapecio.\n");
     printf("3.Calcular el area de un rectangulo.\n");
-    printf("Que desea saber???");scanf("%i",&x);
+    printf("Que desea saber???");
+    scanf("%i", &x);
     switch (x)
     {
     case 1:
-        printf("Ingrese la base primero y despues la haltura: "); scanf("%i%i",&b,&h);
-        z = (b*h)/2;
-        printf("\nEl area del triangulo es: %.2f",z);
+        printf("Ingrese la base primero y despues la haltura: ");
+        scanf("%i%i", &b, &h);
+        z = (b * h) / 2;
+        printf("\nEl area del triangulo es: %.2f", z);
         break;
     case 2:
-        printf("Ingrese primero la base mayor, despue la base menor y al final la haltura: ");scanf("%i%i%i",&B,&b,&h);
-        z= ((B+b)*h)/2;
-        printf("\nEl area del trapecio es de: %.2f",z);
+        printf("Ingrese primero la base mayor, despue la base menor y al final la haltura: ");
+        scanf("%i%i%i", &B, &b, &h);
+        z = ((B + b) * h) / 2;
+        printf("\nEl area del trapecio es de: %.2f", z);
         break;
     case 3:
-        printf("Ingrese el largo y despues el ancho del rectangulo: ");scanf("%i%i",&b,&h);
-        z = b*h ;
-        printf("\nEl area del rectangulo es: %.2f",z);
+        printf("Ingrese el largo y despues el ancho del rectangulo: ");
+        scanf("%i%i", &b, &h);
+        z = b * h;
+        printf("\nEl area del rectangulo es: %.2f", z);
         break;
     default:
         break;
@@ -32,3 +37,4 @@ int main(){
 
     return 0;
 }
+// hola mundo :)

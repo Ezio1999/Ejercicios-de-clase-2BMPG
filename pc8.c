1511f0159b5467aacd912ec09c8030f556e036f0
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-02 10:39:48 -0600

    echo Hello World

---
+++ pc8.c
@@ -0,0 +1,11 @@
+/*Fecha 2/05/2024 
+1.-Escribir los numeros enteros 1-100*/
+#include<stdio.h>
+int main(){
+    int i;
+    for (i=0; i=100; i++){
+        printf("%i",i);
+    }
+
+    return 0;
+}

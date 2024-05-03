1ef8f53e5ace989ba3b4c8c44977a60859c26537
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-03 11:53:27 -0600

    ejercicio 4 root.v2

--- pc11.c
+++ pc11.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main(){
+    char u[50];
     int c,cont=0;
+    printf("Ingrese el usuario: ");scanf("%s",&u);
     do
     {
         printf("Ingrese la contraseña: ");scanf("%d",&c);
         if (c == 1234 )
         {
             printf("Bienvenido al sistema :)");
+            cont = 4;
         }
         cont = cont+1;
-    } while (cont <= 3);
-    printf("Numero de intentos agotado");
+    } while (cont == 3);
+    if (cont==3){ 
+        printf("Numero de intentos agotado");
+    }
 
     return 0;
 }

5656a6c7ebcb4c96f3977e2ca7ae46346c9cb405
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-03 10:52:55 -0600

    ejercicio con contador y acumulador

--- pc10.c
+++ pc10.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main(){
     int x=1,cont,acum;
-    while (x>0)
+    do
     {
         printf("Ingrese un numero: "); scanf("%d",&x);
         printf("Has introducido el numero: %d\n",x);
-        cont = cont+1;
-        acum = acum+x;
-        
-    }
-    printf("Introduciste %d numeros.\n",cont);
-    printf("La suma de tus numeros es: %d",acum);
-    printf("Ejecucion finalizada ");
+        if (x>0)
+        {
+            cont++;
+            acum = acum +x; 
+        }
+    
+    } while (x>0);
+        printf("Introduciste %d numeros.\n",cont);
+        printf("La suma de tus numeros es: %d\n",acum);
+        printf("Ejecucion finalizada ");
     
     return 0;
 }

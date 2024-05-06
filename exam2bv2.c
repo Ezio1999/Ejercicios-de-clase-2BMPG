bf6215f8a203efa4dcf6ba7a5d444900c49a8cd8
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-06 10:57:23 -0600

    ejercicio de examen bloque 2 v2 con while :)

---
+++ exam2bv2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int mayor=0, menor=0, z=0, i=0;
+    int x;
+    while (i<10)
+    {
+        printf("Ingrese el numero %d: ",i+1);scanf("%d",&x);
+        i++;
+        if(x>mayor){
+            mayor = x;
+        }
+        else if(x<menor){
+            menor = x;
+        }
+        z+=x;
+    }
+    printf("La suma de los numeros es de: %d\n", z);
+    printf("El mayor es: %d\n", mayor);
+    printf("El menor es: %d\n", menor);
+
+    return 0;
+}

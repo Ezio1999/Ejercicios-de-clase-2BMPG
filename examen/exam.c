e436ad49016b0a5965eb5b79936f31add76846e5
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-13 09:34:17 -0600

    ejercicio de examen version 1 en comentario

---
+++ examen/exam.c
@@ -0,0 +1,17 @@
+// del programa 
+/*#include <stdio.h>
+int main(){
+    int x,y,z;
+    printf("Ingrese 3 numeros: "); scanf("%i%i%i",&x,&y,&z);
+    if (x == 1 && y == 2 && z == 3)
+    {
+        printf("Acceso permitido :)");
+    }
+    else{
+        printf("Acceso denegado :(");
+    }
+
+    return 0;
+}*/
+
+//v2

efec4d19799149ebc3cf031091560ec6605f88aa
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-13 17:21:08 -0600

    ejercicio de examen version 4 con funciones corregido

--- examen/examv3.c
+++ examen/examv3.c
@@ -1,67 +1,82 @@
 // v3 con funciones
 #include <stdio.h>
 #include <string.h>
-int sistemacontraseña(char monbre[], char contra[], char conv[], int intentos)
-{
-    printf("Ingrese la contraseña:");
-    scanf("%d", &contra);
-    if (strcmp(contra, conv) == 0)
-    {
-        printf("\nBienvenido al sistema :)");
+
+int acum = 0;
+
+// Funcion de contraseña
+int contraSistem(char nombre[], char contra[], char conv[], int *intentos) {
+    printf("\nIngrese su contraseña: ");
+    scanf("%s", contra);
+    if (strcmp(contra, conv) == 0) {
+        printf("\nBienvenido al sistema %s :)", nombre);
+        return 1;
+    } else {
+        (*intentos)--;
+        printf("Contraseña incorrecta. Le quedan %d intentos", *intentos);
+
+        if (*intentos == 0) {
+            printf("Acceso denegado ");
+            return 0; 
+        }
+
+        return 2; 
     }
-    return 1;
-    if(intentos == 0){
-        printf("Acceso denegado");
-        return 0;
-    }
-    return 2;
 }
 
-int calcularprecio(art,acum){
-    for(int i=1;i<=art;i++){
-            printf("¿Cual es el precio del articulo?");
-            scanf("%d",&x);
-            if(x>0){
-                acum += x;
-            }
-            else{
-                printf("Ingrese de nuevo el precio \n");
-                i--;
-            }
+// Funcion para calcular precio
+int calculadora(int art, int acum) {
+    int x;
+    for (int i = 1; i <= art; i++) {
+        printf("¿Cual es el precio del articulo %i?", i);
+        scanf("%d", &x);
+
+        if (x > 0) {
+            acum += x; 
+        } else {
+            printf("Ingrese de nuevo el precio \n");
+            i--; 
+        }
     }
-    return acum;
+
+    return acum; // Return the total price
 }
 
-int main()
-{
+//funcion principal
+int main() {
     char nombre[50];
     char contra[50];
-    char conv[20] = "2b27";
-    int art, x, acum = 1;
+    char conv[20] = "2b27"; 
+    int art;
     int intentos = 3;
+    int result;
+
     printf("Ingrese su nombre: ");
     gets(nombre);
-    while (intentos > 0)
-    {
-        int result == sistemacontraseña(nombre,contra,conv,intentos);
-        if (resul == 1){
-            printf("¿Cuantos articulos llevas?");
-            scanf("%d",&art);
-            if(art > 0){
-                acum = calcularprecio(art,acum);
-                break;
-            }else{
-                printf("le quedan %d",intentos);
-                if(intentos == 0){
-                    printf("Acceso denegado");
+
+    while (intentos > 0) {
+        result = contraSistem(nombre, contra, conv, &intentos);
+
+        if (result == 1) { 
+            printf("\n¿Cuantos articulos llevas?");
+            scanf("%d", &art);
+
+            if (art > 0) {
+                acum = calculadora(art, acum); 
+                break; 
+            } else {
+                printf("Le quedan %d intentos", intentos);
+
+                if (intentos == 0) {
+                    printf("\nAcceso denegado ");
                 }
             }
         }
     }
-        if (acum >= 2){
-            printf("\n Su factura es de: $%d",acum);
-        }
-    
+
+    if (acum >= 1) { 
+        printf("\nSu factura es de: $%d", acum);
+    }
 
     return 0;
 }
